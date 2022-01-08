b1e238de3577667934818cde996118bd53c1f268
AymanShe <aymalsh@gmail.com>
2022-01-08 16:35:58 -0500

    initial commit

---
+++ WebAPI/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName ="nvarchar(100)")]
+        public string FullName { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Country { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string State { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string City { get; set; }
+
+    }
+}
---
+++ WebAPI/Models/PalitaxDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class PalitaxDbContext:DbContext
+    {
+        public PalitaxDbContext(DbContextOptions<PalitaxDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
